3b061985ffd530901f26b2dd4d9c4f8763b84706
SubaShreeDevops <suba.devops@gmail.com>
2026-03-15 22:03:15 +0530

    Create node_exporter.sh

---
+++ scripts/node_exporter.sh
@@ -0,0 +1,7 @@
+wget https://github.com/prometheus/node_exporter/releases/download/v1.7.0/node_exporter-1.7.0.linux-amd64.tar.gz
+
+tar xvf node_exporter*.tar.gz
+
+cd node_exporter*
+
+./node_exporter &
