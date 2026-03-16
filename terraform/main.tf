2361c7341cea90656211b8cc640d540b3c0416c0
SubaShreeDevops <suba.devops@gmail.com>
2026-03-16 11:45:53 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,5 +1,17 @@
+data "aws_ami" "ubuntu" {
+  most_recent = true
+
+  owners = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+}
+
 resource "aws_instance" "devops_ec2" {
-  ami           = "ami-053b0d53c279acc90"
+
+  ami           = data.aws_ami.ubuntu.id
   instance_type = "t2.micro"
 
   key_name = "devops-key"
@@ -31,12 +43,12 @@ resource "aws_security_group" "web_sg" {
 
   egress {
     from_port = 0
-    to_port = 0
-    protocol = "-1"
+    to_port   = 0
+    protocol  = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
 
 resource "aws_s3_bucket" "logs" {
-  bucket = "task-tracker-devops-logs"
+  bucket = "task-tracker-devops-logs-12345"
 }
